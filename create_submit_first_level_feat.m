fd59afb81e5b1b12103a145314c739fdf7c79f02
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-05-02 17:04:02 -0400

    added create_submit_first_level_feat
    also, moved templ2subject
    
    Signed-off-by: Giulia Frazzetta <giulia.frazzetta@gmail.com>

---
+++ create_submit_first_level_feat.m
@@ -0,0 +1,16 @@
+function create_submit_first_level_feat (session_dir,subj_name,numRuns)
+
+% creates a simple script that calls feat for each run first level stat fsf file.
+
+out_dir = fullfile (session_dir, 'first_level_stat');
+if ~isdir(out_dir)
+    mkdir(out_dir);
+end
+fname = fullfile(out_dir, [subj_name 'submit_first_level_feat.sh']);
+fid = fopen(fname,'w');
+fprintf(fid,'#!/bin/bash\n');
+for rr = 1:length(numRuns)
+ fprintf(fid, ['feat ' out_dir '/Run_%02d_5mm.fsf\n'],rr);
+ fprintf(fid, ['feat ' out_dir '/Run_%02d_raw.fsf\n'],rr);
+end
+ 
