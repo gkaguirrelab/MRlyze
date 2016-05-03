f12e6aefb030a2158c33c6ed2d9a371033b6ec33
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-05-03 11:43:46 -0400

    Added FIR_first_level_feat
    Signed-off-by: Giulia Frazzetta <giulia.frazzetta@gmail.com>

---
+++ FIR/FIR_first_level_feat.m
@@ -0,0 +1,54 @@
+function FIR_first_level_feat(outFile,funcVol,anatVol,EVs,condition)
+
+% Write a .fsf file for first-level feat for the HCLV_Photo_7T protocol
+%
+%   Usage:
+%   feat_HCLV_Photo_7T(outFile,funcVol,anatVol,EVs,WA)
+%
+%   Inputs:
+%   outFile     = name of output .fsf file (fullpath)
+%   funcVol     = name of input functional volume (fullpath)
+%   anatVol     = name of 'standard' image for registration (fullpath)
+%   EVs         = structure containing EV text files for each condition (fullpath)
+%   WA          = 1 - wrap around block at beginning of run; 0 - no wrap around
+%
+%   Written by Andrew S Bock Apr 2016
+
+%% Set defaults
+design_dir = fullfile(data_dir, 'Matlab/gkaguirrelab/MelanopsinMR/FEAT_templates');
+%% Load in template
+switch condition
+    case 'MelPulses_400pct'
+        design_file = fullfile(design_dir,'FIR_MaxMel_template.fsf');
+    case 'LMSPulses_400pct'
+        design_file = fullfile(design_dir,'FIR_MaxLMS_template.fsf');
+    case 'SplatterControl'
+        design_file = fullfile(design_dir,'FIR_SplatterControl_template.fsf');
+end
+%% Load functional volume
+tmp = load_nifti(funcVol);
+%% Set design values
+DESIGN.TR = num2str(tmp.pixdim(5)/1000); % TR is in msec, convert to sec
+if tmp.pixdim(5) < 100 % use 100, in case very short TR is used (i.e. multi-band)
+    error('TR is not in msec');
+end
+DESIGN.VOLS = num2str(tmp.dim(5));
+DESIGN.STANDARD = anatVol;
+DESIGN.TOTAL_VOXELS = num2str(tmp.dim(2)*tmp.dim(3)*tmp.dim(4)*tmp.dim(5));
+DESIGN.FEAT_DIR = funcVol;
+for i = 1:length(EVs)
+    eval(['DESIGN.EV' num2str(i) ' = ''' EVs{i} ''';']);
+end
+disp(DESIGN);
+fin = fopen(design_file,'rt');
+fout = fopen(outFile,'wt');
+fields = fieldnames(DESIGN);
+while(~feof(fin))
+    s = fgetl(fin);
+    for f = 1:length(fields)
+        s = strrep(s,['DESIGN_' fields{f}],DESIGN.(fields{f}));
+    end
+    fprintf(fout,'%s\n',s);
+    %disp(s)
+end
+fclose(fin);
--- create_submit_first_level_feat.m
+++ create_submit_first_level_feat.m
@@ -2,7 +2,8 @@ function create_submit_first_level_feat (session_dir,subj_name,numRuns)
 
 % creates a simple script that calls feat for each run first level stat fsf file.
 
-out_dir = fullfile (session_dir, 'first_level_stat');
+out_dir = fullfile (session_dir, 'shell_scripts');
+FSF_dir = fullfile(session_dir,'first_level_stat');
 if ~isdir(out_dir)
     mkdir(out_dir);
 end
@@ -10,7 +11,6 @@ fname = fullfile(out_dir, [subj_name 'submit_first_level_feat.sh']);
 fid = fopen(fname,'w');
 fprintf(fid,'#!/bin/bash\n');
 for rr = 1:length(numRuns)
- fprintf(fid, ['feat ' out_dir '/Run_%02d_5mm.fsf\n'],rr);
- fprintf(fid, ['feat ' out_dir '/Run_%02d_raw.fsf\n'],rr);
+ fprintf(fid, ['feat ' FSF_dir '/Run_%02d_5mm.fsf\n'],rr);
+ fprintf(fid, ['feat ' FSF_dir '/Run_%02d_raw.fsf\n'],rr);
 end
- 
