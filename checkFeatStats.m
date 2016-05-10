f68787d8e633673ba6e7f2d9e5c57ac0872840ac
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-05-10 15:07:53 -0400

    added function 'checkFeatStats'

---
+++ checkFeatStats.m
@@ -0,0 +1,20 @@
+function [statExist] = checkFeatStats(boldDir,featName)
+
+% Checks for the existence of a 'zstat1.nii.gz' file in the stats
+% directory, to serve confirm feat ran correctly
+%
+%   Usage:
+%   [statExist] = checkFeatStats(boldDir,featName);
+%
+%   Output:
+%   statExist = 1 if exist, 0 if not
+%
+%   Written by Andrew S Bock May 2016
+
+%% Check for zstat1
+tmp = exist(fullfile(boldDir,[featName '.feat'],'stats','zstat1.nii.gz'),'file');
+if tmp==0
+    statExist = 0;
+else
+    statExist = 1;
+end
