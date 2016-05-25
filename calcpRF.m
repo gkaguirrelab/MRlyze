6f845691d255fd5bef986dd15e652b810cc7c0f1
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-05-25 00:33:18 -0400

    calcpRF - updates

--- calcpRF.m
+++ calcpRF.m
@@ -19,28 +19,29 @@ end
 [matSrcInds] = break_up_matrix(length(srcInds),1000);
 
 %% Preallocate structure
-prfs(length(srcInds)).co        = [];
-prfs(length(srcInds)).cox       = [];
-prfs(length(srcInds)).coy       = [];
-prfs(length(srcInds)).cosig     = [];
-prfs(length(srcInds)).copeakt   = [];
-prfs(length(srcInds)).copol     = [];
-prfs(length(srcInds)).coecc     = [];
+prfs.co             = nan(length(srcInds),1);
+prfs.cox            = nan(length(srcInds),1);
+prfs.coy            = nan(length(srcInds),1);
+prfs.cosig          = nan(length(srcInds),4);
+prfs.copeakt        = nan(length(srcInds),1);
+prfs.copol          = nan(length(srcInds),1);
+prfs.coecc          = nan(length(srcInds),1);
+
 %% Find best pRF
 progBar = ProgressBar(length(matSrcInds),'finding pRFs...');
 for i = 1:length(matSrcInds)
     % Define source indices
     srctc = tmp.vol(matSrcInds{i},:)';
     % Find best pRF
-    tmp_co                  = corr(srctc,predTCs);
-    [prfsco,prfscoseed]     = max(tmp_co,[],2);
-    prfs(matSrcInds{i}).co                 = prfsco;
-    prfs(matSrcInds{i}).cox                = stimX0(prfscoseed);
-    prfs(matSrcInds{i}).coy                = stimY0(prfscoseed);
-    prfs(matSrcInds{i}).cosig              = stimSig(prfscoseed,:);
-    prfs(matSrcInds{i}).copeakt            = peakHRF(prfscoseed)';
+    tmp_co                              = corr(srctc,predTCs);
+    [prfsco,prfscoseed]                 = max(tmp_co,[],2);
+    prfs.co(matSrcInds{i})              = prfsco;
+    prfs.cox(matSrcInds{i})             = stimX0(prfscoseed);
+    prfs.coy(matSrcInds{i})             = stimY0(prfscoseed);
+    prfs.cosig(matSrcInds{i})           = stimSig(prfscoseed,:);
+    prfs.copeakt(matSrcInds{i})         = peakHRF(prfscoseed)';
     % Convert from cartesian to polar
-    [prfs(matSrcInds{i}).copol,prfs(matSrcInds{i}).coecc] = cart2pol(prfs.cox,prfs.coy);
+    [prfs.copol(matSrcInds{i}),prfs.coecc(matSrcInds{i})] = cart2pol(prfs.cox,prfs.coy);
     progBar(i);
 end
 %% Save data
