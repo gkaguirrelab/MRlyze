a02b33ee36a507c3a2d903babd35a87d689c4d98
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-06-15 10:48:57 -0400

    Updates

--- FEAT/create_EVs_struct.m
+++ FEAT/create_EVs_struct.m
@@ -1,7 +1,11 @@
 function EVs = create_EVs_struct (EVs_number,condition)
 
-% create a struct file that is used to generate the EVs portion of a fsf
+% creates a struct file that is used to generate the EVs portion of a fsf
 % file for FEAT analysis in FSL.
+% For an explaination of the fields in the struct, refer to the #text
+% portion in write_fsf_template (or in any fsf file).
+
+% June 2016 - written (GF)
 
 
 switch condition
@@ -23,13 +27,13 @@ switch condition
         
     case 'FIR'
         for ev = 1:(EVs_number)
-            EVs.title{ev} = ['DESING_EV_TITLE' (num2str(ev,'%02d'))];
+            EVs.title{ev} = ['DESING_EV_TITLE' (num2str(ev,'%03d'))];
             EVs.shape(ev) = 3;
             EVs.convolve(ev) = 0;
             EVs.convolve_phase(ev) = 0;
             EVs.tempfilt_yn(ev) = 0;
             EVs.deriv_yn(ev) = 0;
-            EVs.custom{ev} = ['DESING_EV' (num2str(ev,'%02d'))];
+            EVs.custom{ev} = ['DESING_EV' (num2str(ev,'%03d'))];
             
             %ortogonalization
             for nn = 1:(EVs_number+1) %note that in the fsf file the nn index starts counting from zero
