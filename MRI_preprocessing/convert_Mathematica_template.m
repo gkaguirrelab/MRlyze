c1ef49003b496e0df7cd967d48290babf037cce6
Andrew Bock <bock.andrew@gmail.com>
2016-11-04 15:02:24 -0400

    [ENH] added scripts to convert Mathematica templates

---
+++ MRI_preprocessing/convert_Mathematica_template.m
@@ -0,0 +1,63 @@
+function convert_Mathematica_template(session_dir,template_dir,tNum)
+
+% Takes the .mgz output from Mathematica, convertes to nii.gz and separates
+%   out the pol, ecc, and areas maps into individual volumes.
+%
+%   Usage: convert_Mathematica_templates(session_dir,template_dir)
+%
+%   Written by Andrew S Bock Jul 2015
+
+%% Set default parameters
+if ~exist('template_dir','var')
+    template_dir = fullfile(session_dir,'pRFs','coarse_model_templates');
+end
+%% Find template files
+t = listdir(fullfile(template_dir,'*.mgz'),'files');
+%% Template fit to pRF ROI
+disp('Creating scripts...');
+for dd = tNum
+    hemi = t{dd}(1:2);
+    % Load in a temporary file to overwrite
+    tmp = load_nifti(fullfile(session_dir,'anat_templates',[hemi '.areas.anat.nii.gz']));
+    % Load the output from Mathematica
+    tmpmgh = load_mgh(fullfile(template_dir,t{dd}));
+    % get template name
+    endname = strfind(t{dd},'.mgz');
+    fname = t{dd}(4:endname-1);
+    % Areas
+    Areas = tmp;
+    Areas.vol = squeeze(tmpmgh(1,:,3))';
+    Areas.vol(Areas.vol==99999) = nan;
+    Areas.vol(abs(Areas.vol)>3) = nan;
+    save_nifti(Areas,fullfile(template_dir,[hemi '.areas.' fname '.nii.gz']));
+    % Pol
+    Pol = tmp;
+    Pol.vol = squeeze(tmpmgh(1,:,1))';
+    Pol.vol(Pol.vol==99999) = nan;
+    Pol.vol(abs(Areas.vol)>3) = nan;
+    Pol.vol = deg2rad(Pol.vol) - pi/2;
+    save_nifti(Pol,fullfile(template_dir,[hemi '.pol.' fname '.nii.gz']));
+    % Ecc
+    Ecc = tmp;
+    Ecc.vol = squeeze(tmpmgh(1,:,2))';
+    Ecc.vol(Ecc.vol==99999) = nan;
+    Ecc.vol(abs(Areas.vol)>3) = nan;
+    save_nifti(Ecc,fullfile(template_dir,[hemi '.ecc.' fname '.nii.gz']));
+    % RIGHT HEMISPHERE
+    if strcmp(hemi,'rh')
+        % convert to rh polar angle
+        polname = fullfile(template_dir,[hemi '.pol.' fname '.nii.gz']);
+        tmp = load_nifti(polname);
+        upper = tmp.vol>=0;
+        lower = tmp.vol<0;
+        tmp.vol(upper) = (tmp.vol(upper) - pi);
+        tmp.vol(lower) = (tmp.vol(lower) + pi);
+        save_nifti(tmp,polname);
+        % fix rh areas
+        areaname = fullfile(template_dir,[hemi '.areas.' fname '.nii.gz']);
+        tmp = load_nifti(areaname);
+        tmp.vol = -tmp.vol;
+        save_nifti(tmp,areaname);
+    end
+end
+disp('done.');
---
+++ MRI_preprocessing/create_convert_Mathematica_template_scripts.m
@@ -0,0 +1,40 @@
+function create_convert_Mathematica_template_scripts(outDir,logDir,session_dir,template_dir,mem)
+
+% Creates shell scripts for running 'convert_Mathematica_template'
+%
+%   Usage:
+%   create_pRF_scripts(outDir,logDir,session_dir,subject_name,runNums,hemis,srcROIs,funcs,mem)
+%
+%   Inputs:
+%   outDir          = output directory for shell scripts
+%   logDir          = log directory for cluster output logs
+%   session_dir     = session directory
+%   template_dir    = directory with the .mgz templates
+%   mem             = memory for cluster (default = 10)
+%
+%   Written by Andrew S Bock May 2016
+
+%% Set defaults
+if ~exist(outDir,'dir')
+    mkdir(outDir);
+end
+if isempty(logDir)
+    logDir = '/data/jet/abock/LOGS';
+end
+if ~exist('mem','var')
+    mem = 10;
+end
+%% Find template files
+t = listdir(fullfile(template_dir,'*.mgz'),'files');
+%% Create run_pRF
+for tt = 1:length(t)
+    job_name = sprintf('convertTemplate.%02d',tt);
+    convert_Mathematica_template(session_dir,template_dir,tNum)
+    matlab_string = (['convert_Mathematica_template(''' session_dir ''',''' ...
+        template_dir ''',' num2str(tt) ');']);
+    create_job_shell(outDir,job_name,matlab_string)
+end
+%% Create submit script
+submit_name = 'submit_convertTemplate';
+job_string = listdir(fullfile(outDir,'*.sh'),'files');
+create_submit_shell(outDir,logDir,submit_name,job_string,mem);
