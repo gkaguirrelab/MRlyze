a25520f5eb5ec0769324b3bc85575072ca1c960d
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-12-16 11:19:55 -0500

    [ENH] calc_visual_angle, calc_distance_from_angle - now work with matrix values

--- MRI_preprocessing/calc_distance_from_angle.m
+++ MRI_preprocessing/calc_distance_from_angle.m
@@ -1,12 +1,12 @@
-function [d] = calc_distance_from_angle(a,f)
+function d = calc_distance_from_angle(a,f)
 
-% Calculates the distance (d) given an angle (a) and focal length (f)
+% Calculates the distance(s) (d) given angle(s) (a) and focal length(s) (f). 
+%   Input angle(s) in radians.
 %
 %   Usage:
 %   [d] = calc_distance_from_angle(a,f)
 %
-%   note: assumes angle is in radians
-%
 %   Written by Andrew S Bock Sep 2015
+
 %%
-d = tan(a/2) * (2*f);
+d = tan(a./2) .* (2.*f);
