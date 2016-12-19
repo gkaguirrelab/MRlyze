32b1583f75a25f9ae79c868baeb1858607c75a23
Andrew Bock <abock@mac-pro.uphs.upenn.edu>
2016-12-19 15:35:33 -0500

    [ENH] imshow3D - added 'isT1' input

--- MRI_preprocessing/imshow3D.m
+++ MRI_preprocessing/imshow3D.m
@@ -1,13 +1,25 @@
-function img2dshow = imshow3D(input_vol,crange)
+function img2dshow = imshow3D(input_vol,crange,isT1)
 % Converts 3D image into a 2D mosaic
 %   Displays with imshow with no blank spaces between image slices.
 %
 % Usage:
-%   img2dshow = imshow3D(input_vol)
+%   img2dshow = imshow3D(input_vol,crange,isT1)
+%
+%   Inputs:
+%       input_vol       - '/full/path/to/inputVol.nii.gz'
+%       crange          - [minVal maxVal] color range (default = [])
+%       isT1            - if 1, assume saggital orientation (default = 0);
 %
 % Originally written by Marta Vidorreta Diaz de Cerio
 %   Updated by Andrew S Bock Feb 2015
 
+%% Set defaults
+if ~exist('crange','var')
+    crange = [];
+end
+if ~exist('isT1','var')
+    isT1 = 0;
+end
 %% Load in 3D nifti
 disp(['Loading ' input_vol '...']);
 tmp = load_nifti(input_vol);
@@ -15,7 +27,11 @@ input_img = tmp.vol;
 disp('done.');
 %% Create 2D mosaic
 disp('Creating 2D mosaic...');
-img = permute(input_img,[2 1 3]);
+if ~isT1
+    img = permute(input_img,[2 1 3]);
+else
+    img = permute(input_img,[1 3 2]);
+end
 n_slices = size(img,3);
 n_cols = ceil(sqrt(n_slices));
 n_rows = ceil(n_slices/n_cols);
@@ -23,7 +39,11 @@ if n_rows*n_cols > n_slices
     n_dif = n_rows*n_cols - n_slices;
     img(:,:,end+1:end+n_dif) = 0;
 end
-img2d = flipud(reshape(img, size(img,1), size(img,2)*size(img,3)));
+if ~isT1
+    img2d = flipud(reshape(img, size(img,1), size(img,2)*size(img,3)));
+else
+    img2d = reshape(img, size(img,1), size(img,2)*size(img,3));
+end
 img2dshow = zeros(size(img,1)*n_rows, size(img,2)*n_cols);
 for irow = 1:n_rows
     I1r = 1 + (irow-1)*size(img,1);
@@ -36,7 +56,7 @@ disp('done.');
 %% Plot the mosaic
 disp(['Plotting 2D mosaic for ' input_vol]);
 figure('units','normalized','position',[0 0 1 1]);
-if ~exist('crange','var')
+if isempty(crange)
     imshow(img2dshow, [min(img2dshow(:)) max(img2dshow(:))]);
 else
     imshow(img2dshow, crange);
